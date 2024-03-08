0d2cb664c030d2985c3d9e6e34bd9a6274148e0c
1NT3lCore <148251080+1NT3lCore@users.noreply.github.com>
2024-03-08 23:18:26 +0300

    Add files via upload

---
+++ bro20percent/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+    int cnt = 0;
+    int* A = new int[10];
+    int* ptrA = A;
+
+    for (int i = 0; i < 10; ++i) {
+        *ptrA = rand() % 100;
+        ptrA++;
+    }
+
+    int sum = 0;
+    int count = 0;
+    ptrA = A;
+    for (int i = 0; i < 10; i++) {
+        if (*ptrA > 0 && *ptrA % 5 == 0) {
+            sum += *ptrA;
+            ++count;
+        }
+        ptrA++;
+    }
+
+    std::cout << "Сгенерированный массив из 10 элементов: ";
+    ptrA = A;
+    for (int i = 0; i < 10; ++i) {
+        std::cout << *ptrA << " ";
+        ptrA++;
+    }
+
+    std::cout << "Сумма положительных элементов кратных 5: " << sum << std::endl;
+    std::cout << "Количество таких элементов: " << count << std::endl;
+    return 0;
+}
